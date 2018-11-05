c788f795f9805d3fa037558c7cb41f4a58642434
Preetham Gujjula <preetham.gujjula@gmail.com>
2018-11-05 20:36:05 +0000

    Cleaned up documentation

--- src/Numeric/Modular.hs
+++ src/Numeric/Modular.hs
@@ -5,11 +5,13 @@
     Maintainer  : preetham.gujjula@gmail.com
     Stability   : experimental
 
-    The @'Mod' m@ type represents a Integer modulo m, i.e., a value in ℤ/mℤ, which enables type-safe modular arithmetic.
+    The @'Mod' m@ type represents a Integer modulo m, i.e., a value in ℤ/mℤ,
+    which enables type-safe modular arithmetic.
 
     This library, especially the 'withMod' function, uses ideas from
-    /Functional Pearl: Implicit Configurations -- or, Type Classes Reflect the Values of Types/ by Oleg Kiselyov and Chung-chieh Shan,
-    available here: <http://okmij.org/ftp/Haskell/tr-15-04.pdf>
+    /Functional Pearl: Implicit Configurations — or, Type Classes Reflect the/
+    /Values of Types/ by Oleg Kiselyov and Chung-chieh Shan, available here:
+    <http://okmij.org/ftp/Haskell/tr-15-04.pdf>.
 
     For example, to perform basic modular computations,
 
@@ -18,15 +20,17 @@
     >>> 15 + 3 :: Mod 7
     4
 
-    Modular reductions are performed implicitly, so modular exponentiation can be performed efficiently.
+    Modular reductions are performed implicitly, so modular exponentiation can
+    be performed efficiently.
 
     >>> 60803790666453028877 ^ 88100461154844882932 :: Mod 39127526509442054532
     33479467020524411041
 
-    Compare this to running @(60803790666453028877 ^ 88100461154844882932) \``mod`\` 39127526509442054532@, which is
-    much less efficient.
+    Compare this to running @(60803790666453028877 ^ 88100461154844882932)
+    \``mod`\` 39127526509442054532@, which is much less efficient.
 
-    The modulus can also be specified at runtime without losing any type safety or efficiency.
+    The modulus can also be specified at runtime without losing any type safety
+    or efficiency.
 
     >>> x = mkMod 10
     >>> y = mkMod 17
@@ -41,12 +45,15 @@
     33479467020524411041
 -}
 
-{-# LANGUAGE DataKinds, TypeFamilies, TypeOperators, GADTs, Rank2Types, ScopedTypeVariables, CPP #-}
+{-# LANGUAGE DataKinds, TypeFamilies, TypeOperators, GADTs, Rank2Types,
+    ScopedTypeVariables, CPP #-}
+
 #ifdef MIN_VERSION_GLASGOW_HASKELL
 #if MIN_VERSION_GLASGOW_HASKELL(8,6,1,0)
 {-# LANGUAGE NoStarIsType #-}
 #endif
 #endif
+
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 
 module Numeric.Modular
@@ -80,10 +87,11 @@ mkMod n = Mod (n `mod` (natVal (Proxy :: Proxy m)))
 withMod :: Integer -> (forall m. (KnownNat m) => Mod m) -> Integer
 withMod k m = reifyInteger k (withModProxy m)
 
-{- Given a polymorphic modular value and a proxy for the modulus Nat, resolve the modular value
-   using the given modulus.
+{- Given a polymorphic modular value and a proxy for the modulus Nat, resolve
+   the modular value using the given modulus.
 -}
-withModProxy :: forall m. KnownNat m => (forall n. (KnownNat n) => Mod n) -> Proxy m -> Integer
+withModProxy :: forall m. KnownNat m
+             => (forall n. (KnownNat n) => Mod n) -> Proxy m -> Integer
 withModProxy k modProxy = (getV (k :: Mod m)) `mod` (natVal modProxy)
 
 {- Get the modulus m as a integer of a value of type Mod m. -}
@@ -94,14 +102,18 @@ getM _ = natVal (Proxy :: Proxy m)
 getV :: forall m. (KnownNat m) => Mod m -> Integer
 getV (Mod k) = k
 
-{- The implementation of "reifyIntegral" from the Implicit Configurations paper adapted
-   to the current context.
+{- The implementation of "reifyIntegral" from the Implicit Configurations paper
+   adapted to the current context.
 -}
 reifyInteger :: Integer -> (forall n. (KnownNat n) => Proxy n -> w) -> w
 reifyInteger 0 f = f (Proxy :: Proxy 0)
 reifyInteger n f
-    | even n    = reifyInteger (n `div` 2) (\(Proxy :: Proxy n) -> f (Proxy :: Proxy (n * 2)))
-    | otherwise = reifyInteger (n - 1)     (\(Proxy :: Proxy n) -> f (Proxy :: Proxy (n + 1)))
+    | even n    = reifyInteger
+                    (n `div` 2)
+                    (\(Proxy :: Proxy n) -> f (Proxy :: Proxy (n * 2)))
+    | otherwise = reifyInteger
+                    (n - 1)
+                    (\(Proxy :: Proxy n) -> f (Proxy :: Proxy (n + 1)))
 
 instance Eq (Mod m) where
     (==) (Mod a) (Mod b) = a == b
